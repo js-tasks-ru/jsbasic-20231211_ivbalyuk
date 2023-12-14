3b59f82eae8097d24a780be8bbf144b2e99be3ee
Irina Balyuk <ivbalyuk@gmail.com>
2023-12-14 20:59:57 +0700

    username validation

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,7 +10,10 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  // ваш код...
+  return (name !== null
+    && name.length >= 4
+    && name.indexOf(' ') === -1);
+  
 }
 
 function sayHello() {
