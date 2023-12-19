35fd1db6c40668d24d66b3afaf141b35159555b8
Irina Balyuk <ivbalyuk@gmail.com>
2023-12-19 14:06:16 +0700

    sumSalary

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,15 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let totalSalary = 0;
+  
+  for (let key in salaries) {
+    let presentValue = salaries[key];
+
+    if (typeof presentValue === 'number' 
+    && String(presentValue) !== 'NaN' 
+    && presentValue !== Infinity 
+    && presentValue !== -Infinity) {
+      totalSalary += presentValue;
+    } 
+  }
+  return totalSalary;
 }
