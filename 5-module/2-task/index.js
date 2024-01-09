56be7c2329be38c235faca346cd405571ca60167
Irina Balyuk <ivbalyuk@gmail.com>
2024-01-09 22:04:43 +0700

    module_5

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,7 @@
 function toggleText() {
-  // ваш код...
+  let toggleButton = document.querySelector(".toggle-text-button");
+  let hiddenDiv = document.querySelector("#text");
+  toggleButton.addEventListener("click", () => {
+    hiddenDiv.hidden = !hiddenDiv.hidden;
+  });
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,22 @@
 function initCarousel() {
-  // ваш код...
+  const carousel = document.querySelector(".carousel");
+  const carouselInner = carousel.querySelector(".carousel__inner");
+  let clickCount = 0;
+  carousel.querySelector(".carousel__arrow_left").style.display = "none";
+  carousel.addEventListener("click", (event) => {
+    const carouselArrow = event.target.closest(".carousel__arrow");
+    if (!carouselArrow) {
+      return;
+    }
+
+    if (carouselArrow.classList.contains("carousel__arrow_right")) {
+      clickCount++;
+    } else if (carouselArrow.classList.contains("carousel__arrow_left")) {
+      clickCount--;
+    }
+
+    carouselInner.style.transform = `translateX(-${carouselInner.offsetWidth * clickCount}px)`;
+    carousel.querySelector(".carousel__arrow_left").style.display = (clickCount > 0) ? "" : "none";
+    carousel.querySelector(".carousel__arrow_right").style.display = (clickCount === 3) ? "none" : "";
+  });
 }
