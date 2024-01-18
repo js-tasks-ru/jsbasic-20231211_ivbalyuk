6fb0a857b420d0c6ab32e80f0e195a81c555cf26
Irina Balyuk <ivbalyuk@gmail.com>
2024-01-18 10:43:52 +0700

    7_module

--- 7-module/2-task/index.js
+++ 7-module/2-task/index.js
@@ -10,20 +10,16 @@ export default class Modal {
     this.modal = createElement(`
     <div class="modal">
     <!--Прозрачная подложка перекрывающая интерфейс-->
-    <div class="modal__overlay"></div>
-    
+    <div class="modal__overlay"></div>  
     <div class="modal__inner">
       <div class="modal__header">
         <!--Кнопк<а закрытия модального окна-->
         <button type="button" class="modal__close">
           <img src="/assets/images/icons/cross-icon.svg" alt="close-icon" />
         </button>
-    
-        <h3 class="modal__title">
-         
+        <h3 class="modal__title">   
         </h3>
       </div>
-    
       <div class="modal__body">
         A сюда нужно добавлять содержимое тела модального окна
       </div>
@@ -35,43 +31,36 @@ export default class Modal {
     const body = document.querySelector("body");
     body.append(this.modal);
     body.classList.add("is-modal-open");
-    this.setTitle();
-    this.setBody();
   }
 
   setTitle(title) {
-    this.modalTitle = title || this.modalTitle;
-    this.modal.querySelector(".modal__title").textContent = this.modalTitle;
+    this.modal.querySelector(".modal__title").textContent = title;
   }
 
   setBody(elem) {
-    this.modalBody = elem || this.modalBody;
     this.modal.querySelector(".modal__body").innerHTML = "";
-    this.modal.querySelector(".modal__body").append(this.modalBody);
+    this.modal.querySelector(".modal__body").append(elem);
   }
 
   close() {
     const body = document.querySelector("body");
     body.classList.remove("is-modal-open");
     this.modal.remove();
+    document.removeEventListener("keydown", this.addKeyDown);
   }
 
   addCloseListener() {
     const modalClose = this.modal.querySelector(".modal__close");
+
     modalClose.addEventListener("click", () => {
       this.close();
     });
 
-    document.addEventListener("keydown", (event) => {
+    this.addKeyDown = (event) => {
       if (event.code == "Escape") {
         this.close();
       }
-    });
-
-    document.removeEventListener("keydown", (event) => {
-      if (event.code == "Escape") {
-        this.close();
-      }
-    });
+    };
+    document.addEventListener("keydown", this.addKeyDown);
   }
 }
